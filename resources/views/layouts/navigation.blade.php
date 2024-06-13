1de396ecd0954a0f5a88557143f338c276dfc855
aziym <azim.bakri2002@gmail.com>
2024-06-13 11:28:24 +0800

    UPDATE

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -16,49 +16,53 @@
                 </div>
             </li>
 
-        <li class="mb-2">
-            <!-- Dark green button around Profile link -->
-            <div class="bg-green-900 rounded-md p-2">
-                <a class="text-white" href="{{ route('profile.edit') }}">Profile</a>
-            </div>
-        </li>
-        <li class="mb-2">
-            <!-- Dark green button around Bulletin link -->
-            <div class="bg-green-900 rounded-md p-2">
-                <a class="text-white" href="#">Bulletin</a>
-            </div>
-        </li>
-        <li class="mb-2">
-            <!-- Dark green button around Student Result link -->
-            <div class="bg-green-900 rounded-md p-2">
-                <a class="text-white" href="{{ route('ManageStudentResult.searchExamList') }}">Student Result</a>
-            </div>
-        </li>
+            @if (Auth::check() && Auth::user()->role === 'parents')
+            <li class="mb-2">
+                <!-- Dark green button around Classroom Management link -->
+                <div class="bg-green-900 rounded-md p-2">
+                    <a class="text-white" href="{{ route('activity.getData') }}">Manage Activity</a>
+                </div>
+            </li>
+            <li class="mb-2">
+                    <!-- Dark green button around Bulletin link -->
+                    <div class="bg-green-900 rounded-md p-2">
+                        <a class="text-white" href="{{ route('bulletins.show') }}">Bulletin</a>
+                    </div>
+                </li>
+            @endif
 
-        <li class="mb-2">
-            <!-- Dark green button around Settings link -->
-            <div class="bg-green-900 rounded-md p-2">
-                <a class="text-white" href="#">Settings</a>
-            </div>
-        </li>
-        </li>
-        <li class="mb-2">
-            <!-- Dark green button around Settings link -->
-            <div class="bg-green-900 rounded-md p-2">
-                <a class="text-white" href="{{ route('activity.getData') }}">Activity</a>
-            </div>
-        </li>
-        <!-- Logout button -->
-        @auth
-        <li class="mb-2">
-            <div class="bg-green-900 rounded-md p-2">
-                <form method="POST" action="{{ route('logout') }}">
-                    @csrf
-                    <button type="submit" class="text-white">Logout</button>
-                </form>
-            </div>
-        </li>
-        @endauth
-    </ul>
-    </div>
-</nav>
+            @if (Auth::check() && Auth::user()->role === 'admin')
+                <li class="mb-2">
+                    <!-- Dark green button around Student Result link -->
+                    <div class="bg-green-900 rounded-md p-2">
+                        <a class="text-white" href="{{ route('ManageStudentResult.searchExamList') }}">Student Result</a>
+                    </div>
+                </li>
+                <li class="mb-2">
+                    <!-- Dark green button around Profile link -->
+                    <div class="bg-green-900 rounded-md p-2">
+                        <a class="text-white" href="{{ route('profile.edit') }}">Manage Profile</a>
+                    </div>
+                </li>
+                <li class="mb-2">
+                    <!-- Dark green button around Settings link -->
+                    <div class="bg-green-900 rounded-md p-2">
+                        <a class="text-white" href="{{ route('activity.getDataAdmin') }}">Manage Activity</a>
+                    </div>
+                </li>
+            @endif
+
+            @if (Auth::check() && Auth::user()->role === 'teachers')
+                <li class="mb-2">
+                    <!-- Dark green button around Classroom Management link -->
+                    <div class="bg-green-900 rounded-md p-2">
+                        <a class="text-white" href="{{ route('activity.getData') }}">Manage Activity</a>
+                    </div>
+                </li>
+                <li class="mb-2">
+                    <!-- Dark green button around Lesson Plans link -->
+                    <div class="bg-green-900 rounded-md p-2">
+                        <a class="text-white" href="{{ route('bulletins.index') }}">Manage Bulletin</a>
+                    </div>
+                </li>
+            @endif
